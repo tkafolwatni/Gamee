eee17a4e4575ef769b96d1620ef251d38a5115af
tkafolwatni <tkafolwatni@gmail.com>
2025-06-03 18:17:32 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,14 @@
-// Utility functions
+let keySequence = [];
+const secretCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight'];
+
+function checkSecretCode(callback) {
+  document.addEventListener('keydown', (e) => {
+    keySequence.push(e.key);
+    if (keySequence.length > secretCode.length) {
+      keySequence.shift();
+    }
+    if (keySequence.join() === secretCode.join()) {
+      callback();
+    }
+  });
+}
